27a0d43bafa1031e8b0e8ece3603bbc532a360bd
xinyi <liutaowj46400@touna.cn>
2017-06-07 14:27:25 +0800

    enum single

--- tassel-study/src/main/java/com/zuoquan/lt/basic/enums/ColorTest.java
+++ tassel-study/src/main/java/com/zuoquan/lt/basic/enums/ColorTest.java
@@ -1,5 +1,9 @@
 package com.zuoquan.lt.basic.enums;
 
+import java.util.EnumMap;
+import java.util.EnumSet;
+import java.util.Map;
+
 /**
  * Created by Administrator on 2017/6/6.
  */
@@ -21,10 +25,26 @@ public class ColorTest {
 
     public static void main(String[] args){
         new ColorTest().printColor(Color.RED);
+        //values
         Color[] colors = Color.values();
         for (Color color : colors) {
             System.out.println(color.getName());
         }
 
+        //EnumSet
+        EnumSet<Color> colorSet = EnumSet.allOf(Color.class);
+
+        for (Color color : colorSet) {
+            System.out.println(color.getName());
+        }
+
+        //EnumMap
+        EnumMap<Color, String> enumMap =  new EnumMap<Color, String>(Color.class);
+
+        enumMap.put(Color.RED, "red");
+        enumMap.put(Color.BULE, "bule");
+        for (Map.Entry<Color, String> entry: enumMap.entrySet()){
+            System.out.println(entry.getKey().name() + ":" + entry.getValue());
+        }
     }
 }
---
+++ tassel-study/src/main/java/com/zuoquan/lt/basic/enums/Singleton.java
@@ -0,0 +1,18 @@
+package com.zuoquan.lt.basic.enums;
+
+/**
+ * Created by Administrator on 2017/6/7.
+ */
+public enum Singleton {
+    /**
+     * 定义一个枚举的元素，它就代表了Singleton的一个实例。
+     */
+
+    uniqueInstance;
+    /**
+     * 单例可以有自己的操作
+     */
+    public void otherMethods(){
+        //功能处理
+    }
+}
