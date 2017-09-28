559ad02339e28adc56376fc28e5353bc805dc693
liutao <liutaowj46400@touna.cn>
2017-09-28 17:14:50 +0800

    添加MQ测试

---
+++ tassel-consumer/src/main/java/com/zuoquan/lt/MessageConsumer.java
@@ -0,0 +1,48 @@
+package com.zuoquan.lt;
+
+import com.alibaba.rocketmq.client.consumer.DefaultMQPushConsumer;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import com.alibaba.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import com.alibaba.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import com.alibaba.rocketmq.client.exception.MQClientException;
+import com.alibaba.rocketmq.common.consumer.ConsumeFromWhere;
+import com.alibaba.rocketmq.common.message.MessageExt;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by liutao on 2017/9/27.
+ */
+public class MessageConsumer {
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("testGroup");
+        consumer.setNamesrvAddr("127.0.0.1:9876");
+        consumer.setConsumeFromWhere(ConsumeFromWhere.CONSUME_FROM_FIRST_OFFSET);
+        consumer.subscribe("testTopic", "*");
+
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> list, ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+                for (MessageExt messageExt : list) {
+                    try {
+                        System.out.println(new Date() + new String(messageExt.getBody(),"UTF-8"));
+                    } catch (UnsupportedEncodingException e) {
+                        if (messageExt.getReconsumeTimes() == 3){
+                            //消息其他方式处理，存储DB或日志
+
+                            return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                        }
+                        e.printStackTrace();
+                        return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+                    }
+                }
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        consumer.start();
+        System.out.println("consumer start...");
+    }
+}
